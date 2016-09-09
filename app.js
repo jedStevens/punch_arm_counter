405e6303a367991d44a755a1bf5265ebe9ea4311
Jed Stevens <jed.w.stevens@gmail.com>
2016-09-09 14:37:01 -0400

    Version 1.0

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+var trump_count = 2796
+var hillary_count = 7899
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.get('/inct', function (req, res) {
+  trump_count += 1;
+  res.send('Hello World!');
+});
+app.get('/inch', function (req, res) {
+  hillary_count += 1;
+  res.send('Hello World!');
+});
+app.get('/get', function (req, res) {
+  res.send([trump_count, hillary_count]);
+});
+app.get('/gett', function (req, res) {
+  res.send(trump_count);
+});
+app.get('/geth', function (req, res) {
+  res.send(hillary_count);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
