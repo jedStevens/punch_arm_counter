f7022950d21b40fda3e78de189fb4c6b479ae2ec
Jed Stevens <jed.w.stevens@gmail.com>
2016-09-10 15:15:37 -0400

    Version 1.2.0

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var trump_count = 2796;
-var hillary_count = 7899;
+var trump_count = 0;
+var hillary_count = 0;
 
 var PORT = process.env.PORT || 6969;
 var HOST = '127.0.0.1';
@@ -10,9 +10,40 @@ var HOST = '127.0.0.1';
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
+
+var fs = require('fs');
+fs.readFile("scores.save", function(err, data) {
+    if(err) {
+        throw err;
+    }
+    data = String(data);
+    
+    var parse_t = "";
+    var parse_h = "";
+
+    for (var i = 0; i < data.length; i++){
+        if ( i >= data.indexOf('\n')){
+            parse_h = parse_h + data.charAt(i);
+        } else {
+            parse_t = parse_t + data.charAt(i);
+        }
+    }
+    
+    trump_hits = parseInt(parse_t);
+    hillary_hits = parseInt(parse_h);
+    console.log("Total punches read: " + data);
+}); 
+
+function saveScores(){
+    fs.writeFile("scores.save", trump_count+"\n"+hillary_count);
+    console.log("Saved Scores");
+    setInterval(saveScores, 30000);
+};
+
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
+    setInterval(saveScores, 30000);
 });
 
 server.on('message', function (message, remote) {
@@ -20,10 +51,32 @@ server.on('message', function (message, remote) {
 
     if (message == "PING"){
         console.log("Ping from: " + remote.address + ':' + remote.port);
-        server.send("PING", remote.port, remote.address);
+        server.send("ACCEPT"+PORT, remote.port, remote.address);
+        return;
+    }
+    
+    if (message == "GET"){
+        server.send("UPDATE"+trump_count+","+hillary_count, remote.port, remote.address);
+        return;
+    }
+    if (message == "T"){
+        trump_count += 1;
+        return;
+    }
+    if (message =="H"){
+        hillary_count += 1;
         return;
     }
     
 });
 
 server.bind(PORT, '0.0.0.0');
+
+
+app.get('/', function (req, res) {
+  res.send('<p>Trump: ' + trump_count + '\n\nHillary: '+hillary_count+'</p>');
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port: ' +PORT);
+});
