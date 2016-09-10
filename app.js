1d22ab2a690e28745bf2a69183f0edc6560795d9
Jed Stevens <jed.w.stevens@gmail.com>
2016-09-10 11:22:12 -0400

    heroku fixes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-var trump_count = 2796
-var hillary_count = 7899
+var trump_count = 2796;
+var hillary_count = 7899;
 
-var PORT = process.env.PORT
+var PORT = process.env.PORT || 6969;
 var HOST = '127.0.0.1';
 
 var dgram = require('dgram');
@@ -16,7 +16,7 @@ server.on('listening', function () {
 });
 
 server.on('message', function (message, remote) {
-    var message_str = String(message)
+    var message_str = String(message);
 
     if (message == "PING"){
         console.log("Ping from: " + remote.address + ':' + remote.port);
