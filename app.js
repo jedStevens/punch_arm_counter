4565ed33df9d5b17e61959ce2876fce0b4b50db4
Jed Stevens <jed.w.stevens@gmail.com>
2016-09-10 11:12:24 -0400

    working udp

--- app.js
+++ app.js
@@ -26,37 +26,4 @@ server.on('message', function (message, remote) {
     
 });
 
-server.bind(PORT, HOST);
-
-
-
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/inct', function (req, res) {
-  trump_count += 1;
-  res.send('Hello World!');
-});
-app.get('/inch', function (req, res) {
-  hillary_count += 1;
-  res.send('Hello World!');
-});
-app.get('/get', function (req, res) {
-  res.send([trump_count, hillary_count]);
-});
-app.get('/gett', function (req, res) {
-  res.send(trump_count);
-});
-app.get('/geth', function (req, res) {
-  res.send(hillary_count);
-});
-
-app.listen(port, function () {
-  console.log('Example app listening on port', port);
-});
-
-*/
-
-
+server.bind(PORT, '0.0.0.0');
