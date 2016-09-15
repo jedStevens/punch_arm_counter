123aa0f96d6fb4c33244c10f4c2bd16124b2b1fb
Jed Stevens <jed.w.stevens@gmail.com>
2016-09-15 14:47:13 -0400

    Heroku postgres database

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var config = {
   user: 'jed_a7076913', //env var: PGUSER
   database: 'a7076913_scores', //env var: PGDATABASE
   password: process.env.PGPASSWORD, //env var: PGPASSWORD
-  host: process.env.DATABASE_URL, // Server hosting the postgres database
+  host: process.env.DATABASE_URL || "1.2.3.4", // Server hosting the postgres database
   port: process.env.PGPORT,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
@@ -15,16 +15,20 @@ var config = {
 
 var pool = new pg.Pool(config);
 
-client.connect(function (err){
-    if (err) { throw err; }
+pool.connect(function (err, client, done){
+    if (err) {
+        console.log(err);
+        throw err;
+    }
     
     client.query('SELECT * from scores', function(err, res){
         done();
         if (err){
+            console.log(err);
             throw err;
         }
         
-        console.log(res.rows[0];
+        console.log(res.rows[0]);
     });
 });
 
@@ -65,46 +69,6 @@ app.get('/inc', function (req, res) {
 app.listen(PORT, function () {
   console.log('Example app listening on port: ' +PORT);
   setInterval(pushScores, 60000);
-
-
-  var c = new Client();
-  c.on('ready', function() {
-    c.get('/public_html/cornhole/scores.save', function(err, stream) {
-      if (err){
-        console.log(err);
-        throw err;
-      }
-      stream.once('close', function() { c.end(); });
-      stream.pipe(fs.createWriteStream('scores.save'));
-      fs.readFile("scores.save", function(err, data) {
-        if(err) {
-            throw err;
-        }
-        data = String(data);
-        
-        var parse_t = "";
-        var parse_h = "";
-    
-        for (var i = 0; i < data.length; i++){
-            if ( i >= data.indexOf('\n')){
-                parse_h = parse_h + data.charAt(i);
-            } else {
-                parse_t = parse_t + data.charAt(i);
-            }
-        }
-        
-        trump_hits = parseInt(parse_t);
-        hillary_hits = parseInt(parse_h);
-        console.log("Total punches read: " + data);
-    }); 
-
-
-
-    });
-
-  });
-  // connect to localhost:21 as anonymous
-  c.connect({host: "blacksocks.me", user : "a7076913", password : "Python95"});
 });
 
 
@@ -114,21 +78,5 @@ function saveScores(){
 };
 
 function pushScores(){
-  saveScores();
-  var Client = require('ftp');
-  console.log("Pushing scores");
-  var c = new Client();
-  c.on('ready', function() {
-    var buffer = new Buffer(trump_count+"\n"+hillary_count, "ascii");
-    c.put('scores.save', '/public_html/cornhole/scores.save', function(err) {
-      if (err){
-        console.log("Error with pushing scores to safety");
-        throw err;
-      };
-      c.end();
-      setInterval(pushScores, 60000);
-      console.log("Pushed Scores to safety");
-    });
-  });
-  c.connect({host: "blacksocks.me", user : "a7076913", password : "Python95"});
+    console.log("save scores here");
 };
