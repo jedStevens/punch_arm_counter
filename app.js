4f6f9cb174025f24b1c642a91b88a43d4350320c
Jed Stevens <jed.w.stevens@gmail.com>
2016-09-15 15:10:39 -0400

    Heroku postgres database

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+  client.query('SELECT table_schema,table_name FROM HEROKU_POSTGRESQL_JADE;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
     });
